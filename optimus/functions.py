90786282a2655834fee29674bb78c9a0866ec592
Nav Aulakh <nav@navaulakh.com>
2018-06-19 17:25:51 -0700

    Added more functions

--- optimus/functions.py
+++ optimus/functions.py
@@ -1,4 +1,5 @@
 import arrow
+import pycountry
 
 __all__ = ['string_literal', 'default_value', 'format_date']
 
@@ -14,3 +15,15 @@ def default_value(value, alternate_value):
 def format_date(value, format):
     date_value = arrow.get(value)
     return date_value.format(format)
+
+
+def boolean(value, true_repr, false_repr):
+    return true_repr if value else false_repr
+
+
+def alpha_2_country(value):
+    try:
+        return pycountry.countries.lookup(value).alpha_2
+    except LookupError:
+        # country_name is empty string, None, or no match found
+        return ''
