98c05697e025624f12d314c1bee6399b0037d9ad
Nav Aulakh <nav@navaulakh.com>
2018-10-02 21:05:38 -0700

    WIP Schema renderer

--- static/components/schema_renderer/Node.vue
+++ static/components/schema_renderer/Node.vue
@@ -1,35 +1,29 @@
 <template>
     <ul>
         <li v-for="(value, key) in schema">
-            <a href="#" v-on:click="highlight(value['id'])">
+            <a href="#" v-on:click="selectionHandler(value['id'])">
                 {{ key }}: {{ value['source'] }} ({{ value['type'] }})
             </a>
 
-            <node :schema="value['items']['properties']" v-if="value['type'] === 'array'"></node>
+            <node :schema="value['items']['properties']"
+                  :selectionHandler="selectionHandler"
+                  v-if="value['type'] === 'array'"></node>
 
-            <node :schema="value['properties']" v-if="value['type'] === 'object'"></node>
+            <node :schema="value['properties']"
+                  :selectionHandler="selectionHandler"
+                  v-if="value['type'] === 'object'"></node>
         </li>
     </ul>
 </template>
 
 <script>
-    import {find} from 'lodash'
-
     export default {
         name: "node",
 
         props: {
             schema: Object,
-            required: true
-        },
-
-        methods: {
-            highlight: function (id) {
-                const result = find(this.schema, {id: id})
-                result.type = "number"
-            }
+            selectionHandler: Function
         }
-
     }
 </script>
 
--- static/components/schema_renderer/SchemaRenderer.vue
+++ static/components/schema_renderer/SchemaRenderer.vue
@@ -1,34 +1,86 @@
 <template>
-    <div>
-        <tree :schema="schema"></tree>
+    <div class="tree">
+        <object-form :instance="instance"
+                     v-if="instance != null"
+                     @close="saveInstance"></object-form>
+
+        <node :schema="schema.properties"
+              :selectionHandler="selectInstance"
+              v-if="schema !== null"></node>
     </div>
 </template>
 
 <script>
-    import {debounce} from 'lodash'
-    import Tree from './Tree.vue'
+
+    import {find} from 'lodash'
+    import Node from './Node.vue'
+    import ObjectForm from './ObjectForm.vue'
 
     const data = {
-        rendered_schema: null,
+        instance: null,
+
+    }
+
+    const findObjectByID = function (schema, id) {
+        const keys = Object.keys(schema)
+
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i]
+            const value = schema[key]
+
+            if (value.type === "object") {
+                return findObjectByID(value.properties, id)
+            } else if (value.type === "array") {
+                return findObjectByID(value.items.properties, id)
+            } else {
+                if (value.id === id) {
+                    return value
+                }
+            }
+
+        }
+
+        return null
     }
 
     export default {
+        name: 'schema-renderer',
 
         props: {
-            schema: Object
-        },
-
-        components: {
-            Tree
+            schema: Object,
+            schema_edited: Function
         },
 
         data: function () {
             return data
+        },
+
+        methods: {
+            selectInstance: function (id) {
+                data.instance = findObjectByID(this.schema.properties, id)
+            },
+            saveInstance: function () {
+                data.instance = null
+                this.$emit('schema_edited', this.schema)
+            }
+        },
+
+        watch: {
+            // schema: function (new_value) {
+            //     data.rendered_schema = {schema: new_value}
+            // }
+        },
+
+        components: {
+            Node,
+            ObjectForm
         }
     }
-
 </script>
 
 <style scoped>
-
+    .tree-list ul {
+        padding-left: 16px;
+        margin: 6px 0;
+    }
 </style>
