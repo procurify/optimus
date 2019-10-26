cde8afadd5248eb52851afd18a2f9f7f98f1f9ad
Vinicius Ronconi <vinicius-ronconi@users.noreply.github.com>
2019-10-25 21:54:35 -0700

    Python 3 migration (#8)
    * Update from python2 to python3
    
    * Update package requirements

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ DESCRIPTION = 'A schema transformation tool.'
 URL = 'https://github.com/procurify/optimus'
 EMAIL = 'nav@navaulakh.com'
 AUTHOR = 'Nav Aulakh'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'jsonschema==2.6.0',
+    'jsonschema==3.1.1',
     'jsonpath-rw==1.4.0',
-    'arrow==0.13.1'
+    'arrow==0.15.2'
 ]
 
 setup(
@@ -22,14 +22,14 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    python_requires='>=2.7',
+    python_requires='>=3.7',
     install_requires=REQUIRED,
     include_package_data=True,
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
